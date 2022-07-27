28b88eb1d56e1aa09fa15afc868a1a7a66d3e34b
beauverger <beauverger.rodolphe@orange.fr>
2022-07-27 11:15:17 +0200

    finish

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
